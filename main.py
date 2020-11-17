a53744ff71a48bce4ec2c5d4509704348ae23d99
juliajacaDAM <jjaca@cifpfbmoll.eu>
2020-11-17 16:31:58 +0100

    añadido procedi. de pintar tablero

--- main.py
+++ main.py
@@ -14,3 +14,19 @@ def juego():
             print ("Vuelve a Introducir")
 
 juego()
+
+
+def imprimirTablero():
+    lista = [[j for j in range(0,10)] for i in range(0,10)]
+
+    letras = ('A |', 'B |', 'C |', 'D |', 'E |', 'F |', 'G |', 'H |', 'I |', 'J |')
+    print('     1  2  3  4  5  6  7  8  9 10')
+    print('---------------------------------')
+
+    for fila in range(len(lista)):
+        print(letras[fila], end=" ")
+        for columna in range(len(lista[fila])):
+            print(f'{fila}{lista[fila][columna]}', end = " ")
+        print(end = "\n")
+
+imprimirTablero()
