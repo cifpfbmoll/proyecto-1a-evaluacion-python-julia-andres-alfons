1187b88bd9900f02134cfe9aff876d16a06f3cb7
AlfonsLorente <alorente@cifpfbmoll.eu>
2020-11-25 19:27:38 +0100

    Disparos

--- main.py
+++ main.py
@@ -190,7 +190,7 @@ def juego():
 juego()
 
 
-#GUARDAR DISPAROS
 def guardarDisparos(lista, disparo): #Se pasa la lista de disparos i el disparo del jugador
-    letraInicio, letraFinal = int(equivalencias.get(disparo[0])), int(disparo[1]-1)#Separar los valores del disparo en dos variables
+    letraInicio, letraFinal = int(equivalencias.get(disparo[0])), int(disparo[1])#Separar los valores del disparo en dos variables
+    letraFinal-=1
     lista[letraInicio][letraFinal] = "D"#Cambiar la posicion de la lista por una D de disparo.
